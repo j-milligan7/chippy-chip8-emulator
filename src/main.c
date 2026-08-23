423b168997f98efd4315a183e16ecce969a2d631
j-milligan7 <jmilligan0720@gmail.com>
2026-08-23 18:53:11 +0100

    tiny fix
    literally one bracket

--- src/main.c
+++ src/main.c
@@ -1,9 +1,22 @@
 #include "chip8.h"
 #include "display.h"
-int main(void){
+#include <stdio.h>
+int main(int argc, char **argv){
+    if (argc > 2) {
+        fprintf(stderr, "Usage is %s <rom_path>\n", argv[0]);
+        return 1;
+    }
     Chip8 chip8;
-
     chip8_init(&chip8);
 
+    if (!chip8_load_rom(&chip8, argv[1])) {
+        return 1;
+    }
 
+    if (!display_init()) {
+        return 1;
+    }
+
+    chip8_loop(&chip8);
+    return 0;
 }
